ce308ab074186a207d5bdb11ae883874c7ab9079
KAZUYA215074 <nrtgntm@me.com>
2019-06-12 14:26:48 +0900

    初級1-2,情報登録画面でのエラー表示が終わりました

--- src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
+++ src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
@@ -1,5 +1,7 @@
 package jp.co.sample.emp_management.form;
 
+import javax.validation.constraints.NotBlank;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
@@ -8,33 +10,43 @@ package jp.co.sample.emp_management.form;
  */
 public class InsertAdministratorForm {
 	/** 名前 */
+	@NotBlank(message="名前を入力して下さい")
 	private String name;
 	/** メールアドレス */
+	@NotBlank(message="メールアドレスを入力して下さい")
 	private String mailAddress;
 	/** パスワード */
+	@NotBlank(message="パスワードを入力して下さい")
 	private String password;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getMailAddress() {
 		return mailAddress;
 	}
+
 	public void setMailAddress(String mailAddress) {
 		this.mailAddress = mailAddress;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	@Override
 	public String toString() {
 		return "InsertAdministratorForm [name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
 				+ "]";
 	}
-	
+
 }
